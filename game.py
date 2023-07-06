4766964edacd2bf8a885774d45d25d5cac657007
vogat <james.voight@me.com>
2023-07-06 09:53:04 -0400

    1

---
+++ game.py
@@ -0,0 +1,30 @@
+tools = [
+    {
+        'name': 'teeth',
+        'cost': 0,
+        'income': 1
+    },
+    {
+        'name': 'rusty scissors',
+        'cost': 5,
+        'income': 5
+    },
+    {
+        'name': 'push lawnmower',
+        'cost': 25,
+        'income': 50
+    },
+    {
+        'name': 'battery-powered lawnmower',
+        'cost': 250,
+        'income': 100
+    },
+    {
+        'name': 'starving students',
+        'cost': 500,
+        'income': 250
+    },
+]
+
+print('LANDSCAPER')
+print('You start off with your teeth and make $1 a day. Call the function store() to buy new tools.')
